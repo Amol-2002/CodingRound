939503acbf5db05cc626af0fab9621bc5edab181
Amol Bagul <amolbagul947@gmail.com>
2026-03-19 19:11:50 +0530

    project

--- BottomNavigation2/app/src/main/java/com/example/bottomnavigation/TeacherPage/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bottomnavigation.TeacherPage.model
-
-data class User(
-    val firstName: String,
-    val lastName: String,
-    val email: String,
-    val image: String
-)
--- BottomNavigation2/app/src/main/java/com/example/bottomnavigation/TeacherPage/model/UserResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bottomnavigation.TeacherPage.model
-
-
-data class UserResponse(
-    val users: List<User>
-)
