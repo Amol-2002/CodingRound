14d032fc9d5f8ff090cb04d660966205860935d0
Amol Bagul <amolbagul947@gmail.com>
2026-03-19 19:13:16 +0530

    Clean Project

--- app/src/main/java/com/example/codinground/model/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.codinground.model
-
-data class User(
-    val firstName: String,
-    val lastName: String,
-    val image: String
-)
--- app/src/main/java/com/example/codinground/model/UserResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.codinground.model
-
-data class UserResponse(
-    val users: List<User>
-)
